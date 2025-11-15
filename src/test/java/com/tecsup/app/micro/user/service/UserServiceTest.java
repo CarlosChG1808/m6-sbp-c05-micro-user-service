ab9bdd8bff90ab008115329bb387fe93c27f8aa7
Carlos Chavez <chavezgomezcarlos@gmail.com>
2025-11-14 22:12:10 -0500

    update test

--- src/test/java/com/tecsup/app/micro/user/service/UserServiceTest.java
+++ src/test/java/com/tecsup/app/micro/user/service/UserServiceTest.java
@@ -51,7 +51,7 @@ class UserServiceTest {
         assertNotNull(realUser);
 
         // hope values, real values
-        assertEquals(ID, realUser.getId());
+        assertEquals(1000, realUser.getId());
         assertEquals(NAME, realUser.getName());
         assertEquals(EMAIL, realUser.getEmail());
 
