0dcfcd665d08282076f9347132580fe3173d1650
Carlos Chavez <chavezgomezcarlos@gmail.com>
2025-11-17 20:57:49 -0500

    update jenkins error

--- src/main/java/com/tecsup/app/micro/user/service/UserService.java
+++ src/main/java/com/tecsup/app/micro/user/service/UserService.java
@@ -22,7 +22,8 @@ public class UserService {
 
         UserEntity entity = userRepository.findById(id).orElse(null);
 
-        return  mapper.toDomain(entity);
+        // return  mapper.toDomain(entity);
+        return null; // para robar error en jenkins
     }
 
     public List<User> getAllUsers() {
